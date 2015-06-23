13b56146605fee7398dd8f904ffaaedf9b0df295
ziyang <ziyang0621@gmail.com>
2015-06-23 14:34:28 -0700

    added scrollable view

--- Zerable/ViewController.swift
+++ Zerable/ViewController.swift
@@ -10,10 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       // view.backgroundColor = UIColor.colorWithRGBHex(0x363544, alpha: 1.0)
+        view.backgroundColor =  UIColor.colorWithRGBHex(0x363544, alpha: 1.0)
+        scrollView.backgroundColor = UIColor.clearColor()
+        contentView.backgroundColor =  UIColor.clearColor()
+        
+        let tap = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func dismissKeyboard() {
+        for view in contentView.subviews {
+            if view.isKindOfClass(UITextField) {
+                (view as! UITextField).resignFirstResponder()
+            }
+        }
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        adjustInsetForKeyboardShow(true, notification: notification)
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        adjustInsetForKeyboardShow(false, notification: notification)
+    }
+    
+    func adjustInsetForKeyboardShow(show: Bool, notification: NSNotification) {
+        let userInfo = notification.userInfo ?? [:]
+        let keyboardFrame = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+        let adjustmentHeight = (CGRectGetHeight(keyboardFrame) -
+            (navigationController?.toolbar == nil ? 0 : CGRectGetHeight(navigationController!.toolbar.frame)) + 20) * (show ? 1 : -1)
+        
+        scrollView.contentInset.bottom += adjustmentHeight
+        scrollView.scrollIndicatorInsets.bottom += adjustmentHeight
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 
     override func didReceiveMemoryWarning() {
